4510c5dc9542df5b535b6228c9452261b9b0fed4
Çalgan Aygün <calgan.aygun@mysa.tc>
2021-07-23 23:30:38 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const algoliasearch = require('algoliasearch');
+const fetch = require('node-fetch');
+const fs = require('fs');
+const client = algoliasearch(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_ADMIN_KEY);
+
+const chapterTypes = ['gdg', 'dsc'];
+
+chapterTypes.forEach(async (chapterType) => {
+    const index = client.initIndex(`${chapterType}_chapters`);
+    const allRegionsData = await (await fetch(`https://${chapterType}.community.dev/api/chapter_region?chapters=true`)).json();
+    
+    let chapters = [];
+    allRegionsData.forEach(regionData => {
+        Array.prototype.push.apply(chapters, regionData.chapters);
+        fs.writeFile(`${chapterType}_chapters.json`, JSON.stringify(chapters), (err) => { if(err){ console.error(err);  return;}});
+    });
+    index.clearObjects();
+    index.saveObjects(chapters, { autoGenerateObjectIDIfNotExist: true });
+})
